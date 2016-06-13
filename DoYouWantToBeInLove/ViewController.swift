c508e29887a71b36a5f49af49b6641037ae8d417
TimothyYeou <timyeou1234@hotmail.com>
2016-06-13 17:14:13 +0800

    Version 1

--- DoYouWantToBeInLove/ViewController.swift
+++ DoYouWantToBeInLove/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func start(sender: AnyObject) {
+        let alert = UIAlertController(title: "想愛ㄇ？", message: "請選擇！", preferredStyle: .ActionSheet)
+        
+        let action1 = UIAlertAction(title: "波多也結衣", style: .Default, handler: nil)
+        let action2 = UIAlertAction(title: "徐若宣", style: .Default, handler: nil)
+        let action3 = UIAlertAction(title: "蔡依林", style: .Cancel, handler: nil)
+        let action = UIAlertAction(title: "Yes", style: .Default){  UIAlertAction in
+            let alert2 = UIAlertController(title: "想跟誰?", message: "請選擇！", preferredStyle: .ActionSheet)
+            alert2.addAction(action1)
+            alert2.addAction(action2)
+            alert2.addAction(action3)
+            self.presentViewController(alert2, animated: true, completion: nil)
+        }
+        let cancel = UIAlertAction(title: "No", style: .Cancel, handler: nil)
+        alert.addAction(action)
+        alert.addAction(cancel)
+        self.presentViewController(alert, animated: true, completion: nil)
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
